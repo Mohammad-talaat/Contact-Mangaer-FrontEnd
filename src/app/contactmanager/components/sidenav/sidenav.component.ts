b600a202996ac2a6d47fc7b517c6e27a0fb557dc
Mohammad Talaat <eng.muhamedtalaat@gmail.com>
2022-07-20 04:45:23 +0200

    add toolbar

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidenavComponent implements OnInit {
 
   constructor() { }
-
+  showFiller:boolean = false;
   ngOnInit(): void {
   }
 
