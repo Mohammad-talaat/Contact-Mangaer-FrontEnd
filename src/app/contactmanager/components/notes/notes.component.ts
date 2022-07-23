9960bf6acdcca78ef2d771af8241344325aa0352
Mohammad Talaat <eng.muhamedtalaat@gmail.com>
2022-07-23 10:31:36 +0200

    add filtering on the notes for each user

--- src/app/contactmanager/components/notes/notes.component.ts
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Note } from '../../models/note';
 
@@ -11,12 +12,14 @@ import { Note } from '../../models/note';
 export class NotesComponent implements OnInit, AfterViewInit {
   @Input() notes!:Note[]
   @ViewChild(MatPaginator) paginator!: MatPaginator
+  @ViewChild(MatSort) sort!:MatSort
   displayedColumns: string[] = ['position', 'title', 'date'];
   dataSource!: MatTableDataSource<Note>
   constructor() { }
 
   ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator
+    this.dataSource.sort = this.sort
   }
   ngOnInit(): void {
     this.dataSource = new MatTableDataSource<Note>(this.notes);
