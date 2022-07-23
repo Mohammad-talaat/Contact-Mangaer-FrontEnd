93e5705400ee16d94f4eda3bd56323c8ded08236
Mohammad Talaat <eng.muhamedtalaat@gmail.com>
2022-07-23 09:57:54 +0200

    add pagination to notes component

--- src/app/contactmanager/components/notes/notes.component.ts
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Note } from '../../models/note';
 
@@ -7,16 +8,24 @@ import { Note } from '../../models/note';
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.scss']
 })
-export class NotesComponent implements OnInit {
+export class NotesComponent implements OnInit, AfterViewInit {
   @Input() notes!:Note[]
+  @ViewChild(MatPaginator) paginator!: MatPaginator
   displayedColumns: string[] = ['position', 'title', 'date'];
   dataSource!: MatTableDataSource<Note>
   constructor() { }
 
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator
+  }
   ngOnInit(): void {
     this.dataSource = new MatTableDataSource<Note>(this.notes);
   }
 
 }
 
+
+function viewchild(MatPaginator: any) {
+  throw new Error('Function not implemented.');
+}
 
